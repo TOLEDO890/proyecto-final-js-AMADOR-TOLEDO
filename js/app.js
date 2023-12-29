cff98a7038d0a36b6f7207a0e195ff2d7ed9c31f
amador toledo <amadortoledo5@gmail.com>
2023-12-29 20:05:57 -0300

    final

--- js/app.js
+++ js/app.js
@@ -1,88 +1,116 @@
-// Función para cargar las opciones de moneda
 async function cargarOpcionesMoneda() {
   try {
-    const apiKey = '5392cd99cef42c11967db405';
+    const apiKey = "5392cd99cef42c11967db405";
     const opcionesUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 
     const response = await fetch(opcionesUrl);
 
     if (!response.ok) {
-      throw new Error(`Error en la solicitud de opciones de moneda. Código de respuesta: ${response.status}`);
+      throw new Error(
+        `Error en la solicitud de opciones de moneda. Código de respuesta: ${response.status}`
+      );
     }
 
     const data = await response.json();
-    const monedas = Object.keys(data.conversion_rates); // Obtener los nombres de las monedas
+    const monedas = Object.keys(data.conversion_rates);
 
-    // Llenar las opciones de moneda para los selects de origen y destino
-    llenarOpcionesMoneda('monedaOrigen', monedas, data.conversion_rates);
-    llenarOpcionesMoneda('monedaDestino', monedas, data.conversion_rates);
+    llenarOpcionesMoneda("monedaOrigen", monedas, data.conversion_rates);
+    llenarOpcionesMoneda("monedaDestino", monedas, data.conversion_rates);
   } catch (error) {
     console.error(`Error al cargar opciones de moneda: ${error.message}`);
   }
 }
 
-// Función para llenar las opciones de moneda en un select
 function llenarOpcionesMoneda(selectId, monedas, conversionRates) {
   const select = document.getElementById(selectId);
-  select.innerHTML = '';
+  select.innerHTML = "";
 
-  monedas.forEach(moneda => {
-    const option = document.createElement('option');
+  monedas.forEach((moneda) => {
+    const option = document.createElement("option");
     option.value = moneda;
-    option.text = `${moneda} - ${conversionRates[moneda]}`; // Agregar valores de las divisas
+    option.text = `${moneda} - ${conversionRates[moneda]}`;
     select.appendChild(option);
   });
 }
 
-// Función para manejar errores
 function manejarError(error) {
   console.error(`Error al convertir moneda: ${error.message}`);
 }
 
-// Función para convertir moneda
 async function convertirMoneda() {
   try {
-    const cantidad = document.getElementById('cantidad').value;
-    const monedaOrigen = document.getElementById('monedaOrigen').value;
-    const monedaDestino = document.getElementById('monedaDestino').value;
-    const apiKey = '5392cd99cef42c11967db405';
+    const cantidad = document.getElementById("cantidad").value;
+    const monedaOrigen = document.getElementById("monedaOrigen").value;
+    const monedaDestino = document.getElementById("monedaDestino").value;
+    const apiKey = "5392cd99cef42c11967db405";
 
     const conversionUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${monedaOrigen}`;
     const conversionResponse = await fetch(conversionUrl);
 
     if (!conversionResponse.ok) {
-      throw new Error(`Error en la solicitud de tasas de cambio. Código de respuesta: ${conversionResponse.status}`);
+      throw new Error(
+        `Error en la solicitud de tasas de cambio. Código de respuesta: ${conversionResponse.status}`
+      );
     }
 
     const conversionData = await conversionResponse.json();
     const tasaCambio = conversionData.conversion_rates[monedaDestino];
     const resultado = cantidad * tasaCambio;
 
-    // Muestra los resultados en un input de solo lectura
     mostrarResultados(cantidad, monedaOrigen, resultado, monedaDestino);
   } catch (error) {
-    // Maneja los errores
     manejarError(error);
   }
 }
 
-// Función para mostrar los resultados
 function mostrarResultados(cantidad, monedaOrigen, resultado, monedaDestino) {
-  const resultadosInput = document.getElementById('resultados');
-  resultadosInput.value = `${cantidad} ${monedaOrigen} es igual a ${resultado.toFixed(2)} ${monedaDestino}`;
+  const resultadosInput = document.getElementById("resultados");
+  resultadosInput.value = `${cantidad} ${monedaOrigen} es igual a ${resultado.toFixed(
+    2
+  )} ${monedaDestino}`;
 }
 
-// Espera a que el DOM esté completamente cargado antes de ejecutar la función para cargar opciones de moneda
-document.addEventListener('DOMContentLoaded', cargarOpcionesMoneda);
+function mostrarGrafico(monedaOrigen, monedaDestino) {
+  const datos = {
+    labels: ["2021", "2022", "2023"],
+    datasets: [
+      {
+        label: `Tasa de Cambio de ${monedaOrigen} a ${monedaDestino}`,
+        data: [209, 346, 1025],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const opciones = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
 
+  const ctx = document.getElementById("miGrafico").getContext("2d");
 
+  const miGrafico = new Chart(ctx, {
+    type: "bar",
+    data: datos,
+    options: opciones,
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const monedaOrigen = "USD";
+  const monedaDestino = "ARS";
+  mostrarGrafico(monedaOrigen, monedaDestino);
+});
 
-
-
-
-
-
-
-  
-  
-  
+document.addEventListener("DOMContentLoaded", cargarOpcionesMoneda);
